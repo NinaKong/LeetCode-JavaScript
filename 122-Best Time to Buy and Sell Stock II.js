627add99c4147ae0a405ba7c8fd745673eb67954
NinaSleephead <kx1214@gmail.com>
2018-02-01 09:34:32 -0700

    Best Time to Buy and Sell Stock II

---
+++ 122-Best Time to Buy and Sell Stock II.js
@@ -0,0 +1,8 @@
+var maxProfit = function(prices) {
+    var maxprofit = 0;
+    for (var i = 1; i < prices.length; i++) {
+        if (prices[i] > prices[i - 1])
+            maxprofit += prices[i] - prices[i - 1];
+    }
+    return maxprofit;
+};
